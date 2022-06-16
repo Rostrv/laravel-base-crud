132d3083ee97996cb41a6ece1a316ba877c27256
Rostrv <trovatorosario91@gmail.com>
2022-06-16 20:24:04 +0200

    ended exercise without bonus

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::get('/comics', 'ComicController@index')->name('comics.index');
+Route::get('/comics/create', 'ComicController@create')->name('comics.create');
+Route::post('/comics', 'ComicController@store')->name('comics.store');
+Route::get('/comics/{comic}', 'ComicController@show')->name('comics.show');
